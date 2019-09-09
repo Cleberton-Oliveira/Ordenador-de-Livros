c6b6c3ed8fe79222aac7d1ea6057b3c19333f7c0
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2019-09-09 14:36:38 -0300

    Ordena por colunas

--- js/ordenador.js
+++ js/ordenador.js
@@ -2,6 +2,7 @@ document.querySelector("#adiconar_dados").addEventListener("click", adicionar_li
 var titulo = document.querySelector("input[name='titulo']");
 var autor = document.querySelector("input[name='autor']");
 var ano = document.querySelector("input[name='ano']");
+var ultima_coluna = -1;
 
 function dados_livro(titulo, autor, ano){
 	this.cria_linha_tabela = function () {
@@ -20,14 +21,53 @@ function adicionar_livros(event){
 	livros.cria_linha_tabela();
 };
 
-function ordenar(valor) {
-	linha_tr = document.querySelectorAll('tbody tr')
-	var values = [].slice.call(linha_tr).map(function(el) {
-	return '<tr>' + el.innerHTML + '</tr>';  
-	});
-	values = values.sort();
-	if(document.getElementById('decrecente').checked){
-	values = values.reverse();
-	}
-	document.querySelector('tbody').innerHTML = values.join('');
-};
+function ordena(coluna, tipo){
+	var coluna_selecionada = coluna;
+	var tabela = document.getElementById("tabela");
+	var tbody = tabela.getElementsByTagName("tbody")[0];
+	var rows = tbody.getElementsByTagName("tr");
+	var linhas = new Array();
+	for(var i=0, len=rows.length; i<len; i++) {
+		linhas[i] = new Object;
+		linhas[i].oldIndex = i;
+		var celltext = rows[i].getElementsByTagName("td")[coluna_selecionada].innerHTML.replace(/<[^>]*>/g,"");
+		var re = tipo.toUpperCase() == "N" ? /[^\.\-\+\d]/g : /[^a-zA-Z0-9]/g;
+		linhas[i].value = celltext.replace(re,"").substr(0,25).toLowerCase();
+		}
+	if (coluna_selecionada == ultima_coluna) { linhas.reverse(); }
+	else {
+		ultima_coluna = coluna_selecionada;
+		switch(tipo.toUpperCase()) {
+			case "N" : linhas.sort(ComparaNumero); break;
+			default  : linhas.sort(ComparaTexto);
+			}
+		}
+	var newTableBody = document.createElement("tbody");
+	for(var i=0, len=linhas.length; i<len; i++) {
+		newTableBody.appendChild(rows[linhas[i].oldIndex].cloneNode(true));
+		}
+	tabela.replaceChild(newTableBody,tbody);
+} 
+
+function ComparaTexto(a,b) {
+	var aval = a.value;
+	var bval = b.value;
+	return( aval == bval ? 0 : (aval > bval ? 1 : -1) );
+} 
+
+function ComparaNumero(a,b) {
+	var aval = /\d/.test(a.value) ? parseFloat(a.value) : 0;
+	var bval = /\d/.test(b.value) ? parseFloat(b.value) : 0;
+	return( aval == bval ? 0 : (aval > bval ? 1 : -1) );
+} 
+
+
+
+
+
+
+
+
+
+
+
